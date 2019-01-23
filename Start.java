ac47d8e64f3e06e72ae45948cd9dded31fafb232
DenisaYT <d.ibrahimovic@live.nl>
2019-01-23 13:20:22 +0100

    een

---
+++ Start.java
@@ -0,0 +1,41 @@
+package feestje;
+
+public class Start {
+
+	public Start() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+abstract class Artiest implements Optreden{
+
+	public Artiest() {
+		
+	}
+
+}
+class Zanger extends Artiest{
+	
+}
+class Goochelaar extends Artiest{
+	
+}
+class Stripper extends Artiest{
+	
+}
+class Clown extends Artiest{
+	
+}
+abstract class Band implements Optreden{
+	
+}
+class Rockband extends Band{
+	
+}
+class Kerkkoor extends Band{
+	
+}
+interface Optreden{
+	
+}
